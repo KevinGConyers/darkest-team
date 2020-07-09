0df4ca851c2ccb2d908fb46cce7baffea5033169
kevingconyers <kevingconyers@gmail.com>
2020-07-09 11:56:37 -0500

    added Flagellant, Shieldbreaker, Musketeer. Ground work for trinket's started

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,4 +1,5 @@
 const herodata = "/data/heroes.json";
+const trnkdata = "/data/trinkets.json";
 let dropdown1 = $('#hero1_select');
 let dropdown2 = $('#hero2_select');
 let dropdown3 = $('#hero3_select');
@@ -27,7 +28,6 @@ dropdown3.prop('selectedIndex', 0);
 dropdown4.prop('selectedIndex', 0);
 
 
-// Populate dropdown with list of provinces
 $.getJSON(herodata, function (data) {
 	$.each(data.heroes, function (key, entry) {
 		 if (key === "Vestal"){
@@ -69,6 +69,10 @@ $.getJSON(herodata, function (data) {
 	document.getElementById('abil2').style.display='block';
 	document.getElementById('abil3').style.display='block';
 	document.getElementById('abil4').style.display='block';
+	generateTrinketSelectionPVP("hero1_select", "trnk1");
+	generateTrinketSelectionPVP("hero2_select", "trnk2");
+	generateTrinketSelectionPVP("hero3_select", "trnk3");
+	generateTrinketSelectionPVP("hero4_select", "trnk4");
 });
 document.getElementById('abil1').style.display='none';
 document.getElementById('abil2').style.display='none';
@@ -92,6 +96,12 @@ function getAbilities(hero_name, callback) {
 		callback(data.heroes[hero_name].abilities);
 	});
 }
+function getPvpTrinkets(hero_name, callback) {
+	$.getJSON(trnkdata, function(data) {
+		callback(data.pvp["General"], data.pvp[hero_name]);
+	});
+}
+
 function generateAbilitySelection(select_id, ability_pane_id) {
 	hero_name = document.getElementById(select_id).value;
 	let ability_pane = $('#'+ability_pane_id);
@@ -121,26 +131,52 @@ function generateAbilitySelection(select_id, ability_pane_id) {
 	} 
 }
 
+function generateTrinketSelectionPVP(select_id, trinket_pane_id) {
+	hero_name = document.getElementById(select_id).value;
+	let trinket_pane = $('#'+trinket_pane_id);
+	trinket_pane.empty();
+	trinket_pane.append($('<select></select>').attr({'id': trinket_pane_id + "-1", "class": "trinket_select", 'name': trinket_pane_id + "-1" }));
+	trinket_pane.append($('<select></select>').attr({'id': trinket_pane_id + "-2", "class": "trinket_select", 'name': trinket_pane_id + "-2" }));
+	for(i = 1; i <= 2; i++) {
+		let cur_select = trinket_pane_id + "-" + i.toString();
+		getPvpTrinkets(hero_name, function(all_trinkets, class_trinkets) {
+			sel = $('#'+cur_select);
+			$.each(all_trinkets, function(key) {
+				//console.log(sel[0]["id"].slice(sel[0]["id"].length - 1));
+				sel.append($('<option></option>').attr({'value': key, 'selected': 'selected'}).text(key));
+			});
+			$.each(class_trinkets, function(key) {
+				//console.log(sel[0]["id"].slice(sel[0]["id"].length - 1));
+				sel.append($('<option></option>').attr({'value': key, 'selected': 'selected'}).text(key));
+			});
+		});
+	}
+}
+
 $('#hero1_select').change(function(){
 	updateImage("hero1_select", "Hero1");
 	generateAbilitySelection("hero1_select", "abil1");
+	generateTrinketSelectionPVP("hero1_select", "trnk1");
 	document.getElementById('abil1').style.display='block';
 })
 $('#hero2_select').change(function(){
 	updateImage("hero2_select", "Hero2");
 	generateAbilitySelection("hero2_select", "abil2");
+	generateTrinketSelectionPVP("hero2_select", "trnk2");
 	document.getElementById('abil2').style.display='block';
 
 })
 $('#hero3_select').change(function(){
 	updateImage("hero3_select", "Hero3");
 	generateAbilitySelection("hero3_select", "abil3");
+	generateTrinketSelectionPVP("hero3_select", "trnk3");
 	document.getElementById('abil3').style.display='block';
 
 })
 $('#hero4_select').change(function(){
 	updateImage("hero4_select", "Hero4");
 	generateAbilitySelection("hero4_select", "abil4");
+	generateTrinketSelectionPVP("hero4_select", "trnk4");
 	document.getElementById('abil4').style.display='block';
 
 })
--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.post('/', function (req, res) {
 
 function getParamsObject(body) {
 	//console.log(body);
-	var info = loadJSON();
+	var hero_info = loadHeroJSON();
+	var trinket_info = loadTrinketJSON();
 	var params = {
 		h1: {
 			name: "",
@@ -83,6 +84,16 @@ function getParamsObject(body) {
 				a4: {
 					name: "",
 					imagesrc: ""
+				}
+			},
+			trinkets: {
+				t1: {
+					name: "",
+					imagesrc: "",
+				},
+				t2: {
+					name: "",
+					imagesrc: "",
 				}
 			}
 		},
@@ -106,6 +117,16 @@ function getParamsObject(body) {
 					name: "",
 					imagesrc: ""
 				}
+			},
+			trinkets: {
+				t1: {
+					name: "",
+					imagesrc: "",
+				},
+				t2: {
+					name: "",
+					imagesrc: "",
+				}
 			}
 		},
 		h3: {
@@ -128,6 +149,16 @@ function getParamsObject(body) {
 					name: "",
 					imagesrc: ""
 				}
+			},
+			trinkets: {
+				t1: {
+					name: "",
+					imagesrc: "",
+				},
+				t2: {
+					name: "",
+					imagesrc: "",
+				}
 			}
 		},
 		h4: {
@@ -149,6 +180,16 @@ function getParamsObject(body) {
 				a4: {
 					name: "",
 					imagesrc: ""
+				}
+			},
+			trinkets: {
+				t1: {
+					name: "",
+					imagesrc: "",
+				},
+				t2: {
+					name: "",
+					imagesrc: "",
 				}
 			}
 		}
@@ -178,83 +219,130 @@ function getParamsObject(body) {
 	let h4a3 = body['abil4-3'];
 	let h4a4 = body['abil4-4'];
 
+	let h1t1 = body['trnk1-1'];
+	let h1t2 = body['trnk1-2'];
+
+	let h2t1 = body['trnk2-1'];
+	let h2t2 = body['trnk2-2'];
+
+	let h3t1 = body['trnk3-1'];
+	let h3t2 = body['trnk3-2'];
+
+	let h4t1 = body['trnk4-1'];
+	let h4t2 = body['trnk4-2'];
+
 	//load hero one info
 	params.h1[name] = h1class;
-	params.h1[imagesrc] = info.heroes[h1class][image];
+	params.h1[imagesrc] = hero_info.heroes[h1class][image];
 
 	params.h1.abilities.a1[name] = h1a1;
 	params.h1.abilities.a2[name] = h1a2;
 	params.h1.abilities.a3[name] = h1a3;
 	params.h1.abilities.a4[name] = h1a4;
 
-	params.h1.abilities.a1[imagesrc] = info.heroes[h1class].abilities[h1a1][image];
-	params.h1.abilities.a2[imagesrc] = info.heroes[h1class].abilities[h1a2][image];
-	params.h1.abilities.a3[imagesrc] = info.heroes[h1class].abilities[h1a3][image];
-	params.h1.abilities.a4[imagesrc] = info.heroes[h1class].abilities[h1a4][image];
+	params.h1.abilities.a1[imagesrc] = hero_info.heroes[h1class].abilities[h1a1][image];
+	params.h1.abilities.a2[imagesrc] = hero_info.heroes[h1class].abilities[h1a2][image];
+	params.h1.abilities.a3[imagesrc] = hero_info.heroes[h1class].abilities[h1a3][image];
+	params.h1.abilities.a4[imagesrc] = hero_info.heroes[h1class].abilities[h1a4][image];
 
-	//load hero one info
-	params.h1[name] = h1class;
-	params.h1[imagesrc] = info.heroes[h1class][image];
+	params.h1.trinkets.t1[name] = h1t1;
+	params.h1.trinkets.t2[name] = h1t2;
 
-	params.h1.abilities.a1[name] = h1a1;
-	params.h1.abilities.a2[name] = h1a2;
-	params.h1.abilities.a3[name] = h1a3;
-	params.h1.abilities.a4[name] = h1a4;
+	if(trinket_info.pvp.General.hasOwnProperty(h1t1)) {
+		params.h1.trinkets.t1[imagesrc] = trinket_info.pvp.General[h1t1][image];
+	} else {
+		params.h1.trinkets.t1[imagesrc] = trinket_info.pvp[h1class][h1t1][image];
+	}
 
-	params.h1.abilities.a1[imagesrc] = info.heroes[h1class].abilities[h1a1][image];
-	params.h1.abilities.a2[imagesrc] = info.heroes[h1class].abilities[h1a2][image];
-	params.h1.abilities.a3[imagesrc] = info.heroes[h1class].abilities[h1a3][image];
-	params.h1.abilities.a4[imagesrc] = info.heroes[h1class].abilities[h1a4][image];
+	if(trinket_info.pvp.General.hasOwnProperty(h1t2)) {
+		params.h1.trinkets.t2[imagesrc] = trinket_info.pvp.General[h1t2][image];
+	} else {
+		params.h1.trinkets.t2[imagesrc] = trinket_info.pvp[h1class][h1t2][image];
+	}
 
 	//load hero two info
 	params.h2[name] = h1class;
-	params.h2[imagesrc] = info.heroes[h2class][image];
+	params.h2[imagesrc] = hero_info.heroes[h2class][image];
 
 	params.h2.abilities.a1[name] = h2a1;
 	params.h2.abilities.a2[name] = h2a2;
 	params.h2.abilities.a3[name] = h2a3;
 	params.h2.abilities.a4[name] = h2a4;
 
-	params.h2.abilities.a1[imagesrc] = info.heroes[h2class].abilities[h2a1][image];
-	params.h2.abilities.a2[imagesrc] = info.heroes[h2class].abilities[h2a2][image];
-	params.h2.abilities.a3[imagesrc] = info.heroes[h2class].abilities[h2a3][image];
-	params.h2.abilities.a4[imagesrc] = info.heroes[h2class].abilities[h2a4][image];
+	params.h2.abilities.a1[imagesrc] = hero_info.heroes[h2class].abilities[h2a1][image];
+	params.h2.abilities.a2[imagesrc] = hero_info.heroes[h2class].abilities[h2a2][image];
+	params.h2.abilities.a3[imagesrc] = hero_info.heroes[h2class].abilities[h2a3][image];
+	params.h2.abilities.a4[imagesrc] = hero_info.heroes[h2class].abilities[h2a4][image];
 
+	if(trinket_info.pvp.General.hasOwnProperty(h2t1)) {
+		params.h2.trinkets.t1[imagesrc] = trinket_info.pvp.General[h2t1][image];
+	} else {
+		params.h2.trinkets.t1[imagesrc] = trinket_info.pvp[h2class][h2t1][image];
+	}
+
+	if(trinket_info.pvp.General.hasOwnProperty(h2t2)) {
+		params.h2.trinkets.t2[imagesrc] = trinket_info.pvp.General[h2t2][image];
+	} else {
+		params.h2.trinkets.t2[imagesrc] = trinket_info.pvp[h2class][h2t2][image];
+	}
 	//load hero three info
 	params.h3[name] = h3class;
-	params.h3[imagesrc] = info.heroes[h3class][image];
+	params.h3[imagesrc] = hero_info.heroes[h3class][image];
 
 	params.h3.abilities.a1[name] = h1a1;
 	params.h3.abilities.a2[name] = h1a2;
 	params.h3.abilities.a3[name] = h1a3;
 	params.h3.abilities.a4[name] = h1a4;
 
-	params.h3.abilities.a1[imagesrc] = info.heroes[h3class].abilities[h3a1][image];
-	params.h3.abilities.a2[imagesrc] = info.heroes[h3class].abilities[h3a2][image];
-	params.h3.abilities.a3[imagesrc] = info.heroes[h3class].abilities[h3a3][image];
-	params.h3.abilities.a4[imagesrc] = info.heroes[h3class].abilities[h3a4][image];
+	params.h3.abilities.a1[imagesrc] = hero_info.heroes[h3class].abilities[h3a1][image];
+	params.h3.abilities.a2[imagesrc] = hero_info.heroes[h3class].abilities[h3a2][image];
+	params.h3.abilities.a3[imagesrc] = hero_info.heroes[h3class].abilities[h3a3][image];
+	params.h3.abilities.a4[imagesrc] = hero_info.heroes[h3class].abilities[h3a4][image];
+
+	if(trinket_info.pvp.General.hasOwnProperty(h3t1)) {
+		params.h3.trinkets.t1[imagesrc] = trinket_info.pvp.General[h3t1][image];
+	} else {
+		params.h3.trinkets.t1[imagesrc] = trinket_info.pvp[h3class][h3t1][image];
+	}
+
+	if(trinket_info.pvp.General.hasOwnProperty(h3t2)) {
+		params.h3.trinkets.t2[imagesrc] = trinket_info.pvp.General[h3t2][image];
+	} else {
+		params.h3.trinkets.t2[imagesrc] = trinket_info.pvp[h3class][h3t2][image];
+	}
 
 	//load hero four info
 	params.h4[name] = h4class;
-	params.h4[imagesrc] = info.heroes[h4class][image];
+	params.h4[imagesrc] = hero_info.heroes[h4class][image];
 
 	params.h4.abilities.a1[name] = h4a1;
 	params.h4.abilities.a2[name] = h4a2;
 	params.h4.abilities.a3[name] = h4a3;
 	params.h4.abilities.a4[name] = h4a4;
 
-	params.h4.abilities.a1[imagesrc] = info.heroes[h4class].abilities[h4a1][image];
-	params.h4.abilities.a2[imagesrc] = info.heroes[h4class].abilities[h4a2][image];
-	params.h4.abilities.a3[imagesrc] = info.heroes[h4class].abilities[h4a3][image];
-	params.h4.abilities.a4[imagesrc] = info.heroes[h4class].abilities[h4a4][image];
+	params.h4.abilities.a1[imagesrc] = hero_info.heroes[h4class].abilities[h4a1][image];
+	params.h4.abilities.a2[imagesrc] = hero_info.heroes[h4class].abilities[h4a2][image];
+	params.h4.abilities.a3[imagesrc] = hero_info.heroes[h4class].abilities[h4a3][image];
+	params.h4.abilities.a4[imagesrc] = hero_info.heroes[h4class].abilities[h4a4][image];
 
+	if(trinket_info.pvp.General.hasOwnProperty(h4t1)) {
+		params.h4.trinkets.t1[imagesrc] = trinket_info.pvp.General[h4t1][image];
+	} else {
+		params.h4.trinkets.t1[imagesrc] = trinket_info.pvp[h4class][h4t1][image];
+	}
+
+	if(trinket_info.pvp.General.hasOwnProperty(h4t2)) {
+		params.h4.trinkets.t2[imagesrc] = trinket_info.pvp.General[h4t2][image];
+	} else {
+		params.h4.trinkets.t2[imagesrc] = trinket_info.pvp[h4class][h4t2][image];
+	}
 
 	console.log(params);
 
 	return params;
 }
 
-function loadJSON() {
+function loadHeroJSON() {
 	// include file system module
 	var fs = require('fs');
 	path = __dirname + "/public/data/heroes.json";
@@ -264,9 +352,16 @@ function loadJSON() {
 	return jsonParsed;
 }
 
-function startServer(port_number) {
-
+function loadTrinketJSON() {
+	// include file system module
+	var fs = require('fs');
+	path = __dirname + "/public/data/trinkets.json";
+	// read file sample.json file
+	var text = fs.readFileSync(path) 
+	var jsonParsed = JSON.parse(text);
+	return jsonParsed;
 }
+
 
 
 
